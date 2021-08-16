748fe29229cd7c7f91f5bf690b3b1f87a37e7db4
will <weihuang@thoughtworks.com>
2021-08-16 16:21:29 +0800

    init

---
+++ src/main/java/com/thoughtworks/training/travisheroku/TravisHerokuApplication.java
@@ -0,0 +1,13 @@
+package com.thoughtworks.training.travisheroku;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravisHerokuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TravisHerokuApplication.class, args);
+    }
+
+}
